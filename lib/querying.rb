b902ae287217d19e49f8b3a7460c63539e37d050
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-06-28 00:31:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,10 @@ def select_name_and_series_subgenres_of_authors
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
-  FROM series"
+  FROM characters 
+  JOIN series 
+  ON character.id = series.character_id
+  ORDER BY 
 end
 
 def select_character_names_and_number_of_books_they_are_in
