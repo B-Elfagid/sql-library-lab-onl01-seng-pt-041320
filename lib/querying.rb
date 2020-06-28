40e1b75148331f9f94e21db426c63339253a4fec
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-06-28 00:53:52 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,14 +28,11 @@ def select_name_and_series_subgenres_of_authors
   end
 
 def select_series_title_with_most_human_characters
-  "SELECT Series.title
-  FROM series
-  INNER JOIN characters 
-  ON series.id = characters.series_id
-  GROUP BY series.title, species
-  HAVING characters.species = 'human'
-  ORDER BY COUNT(species) DESC
-  LIMIT 1;"
+  "SELECT title FROM series
+   JOIN characters ON series.author_id = characters.author_id
+   WHERE characters.species = 'human'
+   GROUP BY series.title
+   ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
